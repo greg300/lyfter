3a10288d52ae67919977235fa9382a48462e62d1
greg300 <greg300@comcast.net>
2021-10-27 05:06:57 -0400

    Migrate command line args to constants

--- src/constants.py
+++ src/constants.py
@@ -2,9 +2,15 @@ MIN_X = 4600
 MAX_X = 5000
 MIN_Y = 8100
 MAX_Y = 8500
-NUM_RIDERS = 100
-AVG_VELOCITY = 1500  # 10 m/hour
 
-# Normal Distribution values
-sigma_R = 30  # Rider std deviation = 700m
+NUM_RIDERS = 100  # Number of Riders within the area at any instant.
+MIN_DRIVERS = 100  # Minimum number of Drivers with which to run a simulation.
+MAX_DRIVERS = 1000  # Maximum number of Drivers with which to run a simulation.
+STEP_DRIVERS = 100  # Step size for iterating over number of Drivers across simulations.
+NUM_ITERATIONS = 5  # Number of iterations for which to test each number of Drivers.
+
+AVG_VELOCITY = 1500  # in units of 10 m/hour
+
+# Normal Distribution values (warning: changing these may result in Riders and Drivers generating out of bounds).
+sigma_R = 30  # Rider std deviation = 300m
 sigma_D = 20  # Driver std deviation = 200m
