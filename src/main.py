596f0fcc884fa24ad48739a0f34f8429b385d298
greg300 <greg300@comcast.net>
2021-10-27 05:07:14 -0400

    Add second "answer" mode via CLA

--- src/main.py
+++ src/main.py
@@ -1,34 +1,50 @@
 import argparse
 
-from constants import NUM_RIDERS
 from simulation import Simulation
 from csvutils import write_locations, write_results
+import constants
+NUM_RIDERS = constants.NUM_RIDERS
+MIN_DRIVERS = constants.MIN_DRIVERS
+MAX_DRIVERS = constants.MAX_DRIVERS
+STEP_DRIVERS = constants.STEP_DRIVERS
+NUM_ITERATIONS = constants.NUM_ITERATIONS
 
 parser = argparse.ArgumentParser(description='Run a ride share simulation.')
-parser.add_argument('--min_drivers', metavar='min_drivers', type=int, nargs='?', default=NUM_RIDERS,
-                    help='minimum number of drivers in the simulation')
-parser.add_argument('--max_drivers', metavar='max_drivers', type=int, nargs='?', default=2*NUM_RIDERS,
-                    help='maximum number of drivers in the simulation')
-parser.add_argument('--step_drivers', metavar='step_drivers', type=int, nargs='?', default=1,
-                    help='step size in iterating from min_drivers to _max_drivers')
-parser.add_argument('num_iterations', metavar='num_iterations', type=int, nargs='+',
-                    help='number of iterations to run each simulation')
+parser.add_argument('mode', metavar='mode', type=int, nargs='?', default=1,
+                    help='mode of operation (1 = results.csv output for analysis, 2 = direct answer)')
 
 args = parser.parse_args()
-num_iterations = args.num_iterations[0]
+mode = args.mode
 
 s = Simulation()
 results = []
 
-for num_drivers in range(args.min_drivers, args.max_drivers, args.step_drivers):
-    riders = None
-    drivers = None
-    for i in range(num_iterations):
-        max_wait,drivers,riders = s.start(num_drivers)
-        results.append([NUM_RIDERS, num_drivers, max_wait])
-    write_locations('locations_%04d_%04d.csv' % (NUM_RIDERS, num_drivers),
-                    drivers,
-                    riders)
+if mode == 1:
+    for num_drivers in range(MIN_DRIVERS, MAX_DRIVERS, STEP_DRIVERS):
+        riders = None
+        drivers = None
+        for i in range(NUM_ITERATIONS):
+            max_wait, drivers, riders = s.start(num_drivers)
+            results.append([NUM_RIDERS, num_drivers, max_wait])
+        #write_locations('locations_%04d_%04d.csv' % (NUM_RIDERS, num_drivers),
+        #               drivers,
+        #              riders)
+    write_results('results.csv', results)
 
-write_results('results.csv', results)
+elif mode == 2:
+    max_wait = 300
+    num_drivers = NUM_RIDERS
+    while max_wait >= 300:
+        iteration_results = []
+        for i in range(NUM_ITERATIONS):
+            max_wait, _, _ = s.start(num_drivers)
+            iteration_results.append(max_wait)
+        max_wait = max(iteration_results)
+        if max_wait >= 300:
+            num_drivers += STEP_DRIVERS
+    print(num_drivers)
+
+else:
+    print("Invalid mode. Please try again.")
+
  
